d7b1ae09fe91baf3ab769795e947d2627a00cade
lgants <lgants@gmail.com>
2017-11-20 20:42:26 -0600

    created new apps

--- simple_django_api/settings.py
+++ simple_django_api/settings.py
@@ -30,14 +30,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+THIRD_PARTY_APPS = (
+    'rest_framework',
+    'todos',
+)
+
+LOCAL_APPS = (
+    'users',
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
